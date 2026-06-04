5375f48092d0388eb990c18b93f71b071959ba2e
VuongLe202-creat <vuonglt2388@ut.edu.vn>
2026-06-04 16:22:35 +0700

    update socket server

--- Code/server/socket_server.py
+++ Code/server/socket_server.py
@@ -1,39 +1,41 @@
-import socket
+import socket 
 
+ 
 
-class SocketServer:
-    def __init__(self, host: str, port: int, backlog: int = 10):
-        self.host = host
-        self.port = port
-        self.backlog = backlog
-        self.sock = None
+class SocketServer: 
+     def __init__(self, host: str, port: int, backlog: int = 10): 
+        self.host = host 
 
-    @property
-    def address(self):
-        return (self.host, self.port)
+        self.port = port 
 
-    def create_socket(self):
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return server_socket
+        self.backlog = backlog 
 
-    def start(self):
-        self.sock = self.create_socket()
-        self.sock.bind(self.address)
-        self.sock.listen(self.backlog)
-        return self.sock
+        self.sock = None 
 
-    def close(self):
-        if not self.sock:
-            return
-        try:
-            self.sock.close()
-        finally:
-            self.sock = None
+ 
 
-    def __enter__(self):
-        self.start()
-        return self
+    def start(self): 
 
-    def __exit__(self, _exc_type, _exc, _traceback):
-        self.close()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+        self.sock.bind((self.host, self.port)) 
+
+        self.sock.listen(self.backlog) 
+
+        return self.sock 
+
+ 
+
+    def close(self): 
+
+        if self.sock: 
+
+             try: 
+
+                 self.sock.close() 
+
+            finally: 
+
+                self.sock = None 
