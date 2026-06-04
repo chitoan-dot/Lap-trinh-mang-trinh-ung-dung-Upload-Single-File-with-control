402d2a46177fc2ceabe64ee4703dbf2815fdf814
Dang Quoc Viet <quocviet21052006@gmail.com>
2026-06-04 17:22:46 +0700

    update socket client

--- Code/client/socket_client.py
+++ Code/client/socket_client.py
@@ -1,6 +1,5 @@
 import socket
 
-
 class SocketClient:
     def __init__(self, host: str, port: int, timeout: float = 10):
         self.host = host
@@ -8,31 +7,18 @@ class SocketClient:
         self.timeout = timeout
         self.sock = None
 
-    @property
-    def address(self):
-        return (self.host, self.port)
-
-    def create_socket(self):
-        return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     def connect(self):
-        self.sock = self.create_socket()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.settimeout(self.timeout)
-        self.sock.connect(self.address)
+        self.sock.connect((self.host, self.port))
         self.sock.settimeout(None)
         return self.sock
 
     def close(self):
-        if not self.sock:
-            return
-        try:
-            self.sock.close()
-        finally:
-            self.sock = None
+        if self.sock:
+            try:
+                self.sock.close()
+            finally:
+                self.sock = None
 
-    def __enter__(self):
-        self.connect()
-        return self
 
-    def __exit__(self, _exc_type, _exc, _traceback):
-        self.close()
